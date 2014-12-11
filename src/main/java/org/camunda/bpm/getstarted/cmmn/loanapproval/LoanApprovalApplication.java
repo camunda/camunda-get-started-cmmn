800a85dd2a751c03488143e79b48d88037bb794b
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2014-12-11 15:27:57 +0100

    Step 4: Add PostDeploy method

--- src/main/java/org/camunda/bpm/getstarted/cmmn/loanapproval/LoanApprovalApplication.java
+++ src/main/java/org/camunda/bpm/getstarted/cmmn/loanapproval/LoanApprovalApplication.java
@@ -16,10 +16,23 @@
  */
 package org.camunda.bpm.getstarted.cmmn.loanapproval;
 
+import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.CaseService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.variable.Variables;
 
 @ProcessApplication("Loan Approval App CMMN")
 public class LoanApprovalApplication extends ServletProcessApplication {
-  //empty implementation
+
+  @PostDeploy
+  public void startCaseInstance(ProcessEngine processEngine) {
+    CaseService caseService = processEngine.getCaseService();
+    caseService.createCaseInstanceByKey("loan_application",
+        Variables.createVariables()
+          .putValue("applicationSufficient", Variables.booleanValue(null))
+          .putValue("rating", Variables.integerValue(null)));
+
+  }
 }
